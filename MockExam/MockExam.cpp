fd5c9324fe79f96967b04d4378e2b122d65df919
david-levi-student <dal0531@my.londonmet.ac.uk>
2022-05-18 12:06:51 +0100

    Streamlining code

--- MockExam/MockExam.cpp
+++ MockExam/MockExam.cpp
@@ -1,37 +1,27 @@
 #include <iostream>
 #include <string>
 
-int check();
-
-using namespace std;
-string word;
-
 int main()
 {
+    using namespace std;
+    string word;
+
     std::cout << "Type in a word: " << std::endl;
     std::cin >> word;
     std::cout << "Here's a fun fact:\n";
     std::cout << "A palindrome is a word that sounds the same read backwards as it does forwards.\n";
     std::cout << "Let's see if your word fits that criteria, shall we?" << std::endl;
     system("PAUSE");
-    check();
-    system("PAUSE");
-}
-
-int check()
-{
-    auto first = word.begin();
-    auto final = word.end();
     
-    if (first == final)
+    if (word.begin() == word.end())
     {
-        std::cout << "This word is a palindrome" << std::endl;
+        std::cout << "Yes! This word is a palindrome." << std::endl;
     }
     
-    else    
+    else
     {
         std::cout << "Nope, sorry." << std::endl;
     }
-
-    return true;
+    
+    system("PAUSE");
 }
