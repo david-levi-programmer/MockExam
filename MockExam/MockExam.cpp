178f7fc0ca001ea23074664473423d3277c9e14e
david-levi-student <dal0531@my.londonmet.ac.uk>
2022-05-18 12:23:17 +0100

    Trying solutions

--- MockExam/MockExam.cpp
+++ MockExam/MockExam.cpp
@@ -6,22 +6,22 @@ int main()
     using namespace std;
     string word;
 
-    std::cout << "Type in a word: " << std::endl;
+    std::cout << "Type in a word, would you? Any word: " << std::endl;
     std::cin >> word;
-    std::cout << "Here's a fun fact:\n";
+    std::cout << "Now, here's a fun fact for you:\n";
     std::cout << "A palindrome is a word that sounds the same read backwards as it does forwards.\n";
     std::cout << "Let's see if your word fits that criteria, shall we?" << std::endl;
     system("PAUSE");
-    
+    //for (auto it = word[0]; it != word.end();)
     if (word.begin() == word.end())
     {
         std::cout << "Yes! This word is a palindrome." << std::endl;
     }
-    
+
     else
     {
         std::cout << "Nope, sorry." << std::endl;
     }
-    
+
     system("PAUSE");
 }
